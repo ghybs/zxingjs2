7c97e053fc946d143e1b1e87d7e0e669d9531754
ghybs <ghybs1@gmail.com>
2016-02-02 17:08:09 +0400

    Added live camera test

--- test/test.js
+++ test/test.js
@@ -8,26 +8,101 @@ var img = document.getElementById("ean8-1_1"),
 canvas.width = width;
 canvas.height = height;
 context.drawImage(img, 0, 0, width, height);
+context.strokeStyle = "#FF0000";
 
 zxing.init(width);
 
 zxing.oneD.codabar.reader.init();
 
-var result = zxing.oneDReader._decode(zxing._getCanvasImageRGBAData(canvas)),
-    lines = result.lines;
+elIdAddListener("decodeImage", "click", function () {
+    context.drawImage(img, 0, 0, width, height);
+    decode();
+});
 
-var li, text, line, result2;
-context.strokeStyle = "#FF0000";
+function decode() {
+    var result = zxing.oneDReader._decode(zxing._getCanvasImageRGBAData(canvas)),
+        lines = result.lines;
 
-for (var i = 0; i < lines.length; i += 1) {
-    line = lines[i];
-    li = document.createElement("li");
-    text = document.createTextNode(line.bits || "(empty)");
-    result2 = document.createTextNode(JSON.stringify(line.result, null, 2));
-    li.appendChild(text);
-    li.appendChild(result2);
-    ul.appendChild(li);
-    //console.log("bitRow length: " + line.bits.length);
+    var li, text, line, result2;
 
-    context.strokeRect(0, line.rowNumber, width, 0);
+    for (var i = 0; i < lines.length; i += 1) {
+        line = lines[i];
+        li = document.createElement("li");
+        text = document.createTextNode(line.bits || "(empty)");
+        result2 = document.createTextNode(JSON.stringify(line.result, null, 2));
+        li.appendChild(text);
+        li.appendChild(result2);
+        ul.appendChild(li);
+        //console.log("bitRow length: " + line.bits.length);
+
+        context.strokeRect(0, line.rowNumber, width, 0);
+    }
+}
+
+var v = getElById("v"),
+    capturing = false;
+
+elIdAddListener("startCam", "click", function () {
+    var n = navigator;
+    console.log("trying to start camera");
+
+    if (n.mediaDevices.getUserMedia) {
+        n.mediaDevices.getUserMedia({video: { facingMode: "environment"} , audio: false})
+            .then(success)
+            .catch(error);
+    } else if (n.webkitGetUserMedia) {
+        n.webkitGetUserMedia({video: true, audio: false}, success, error);
+    } else if(n.mozGetUserMedia) {
+        n.mozGetUserMedia({video: true, audio: false}, success, error);
+    } else if(n.getUserMedia) {
+        n.getUserMedia({video: true, audio: false}, success, error);
+    } else {
+        console.log("No camera support found");
+    }
+});
+
+function captureToCanvas() {
+    if (capturing)
+    {
+        try {
+            context.drawImage(v, 0, 0);
+            try {
+                decode();
+            }
+            catch (e) {
+                console.log(e);
+                setTimeout(captureToCanvas, 500);
+            }
+        } catch(e){
+            console.log(e);
+            setTimeout(captureToCanvas, 500);
+        }
+    }
+}
+
+function success(stream) {
+    v.src = window.URL.createObjectURL(stream);
+    capturing = true;
+    console.log("Camera started!");
+    setTimeout(captureToCanvas, 500);
+}
+
+function error(error) {
+    capturing = false;
+    console.log("No camera available");
+}
+
+function elIdAddListener(id, listenerType, listenerCallback) {
+    var el = getElById(id);
+
+    if (el) {
+        el.addEventListener(listenerType, listenerCallback);
+        return true;
+    }
+
+    return false;
+}
+
+function getElById(id) {
+    return document.getElementById(id);
 }
